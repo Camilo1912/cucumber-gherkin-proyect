c9068630ee9ad4621e591d7f94539ec73d3c7e2c
camilo1912 <camilomarilafm@gmail.com>
2025-10-27 22:23:18 -0300

    Example of filter using Vuelos TAG

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -12,6 +12,6 @@ import org.junit.runner.RunWith;
         "pretty",
         "html:target/cucumber-reports/reporte.html",
     },
-    tags = "" 
+    tags = "@Vuelos" 
 )
 public class TestRunner {}
